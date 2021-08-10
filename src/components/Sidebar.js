89ca21f4381431fd50633c8a03456f170065d909
rodrigoemanuelde <rodrigoemanuelde@hotmail.com>
2021-08-09 21:35:57 -0300

    first commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+const Sidebar = ({ topAnime }) => {
+  return (
+    <aside>
+      <nav>
+        <h3>Top Anime</h3>
+        {topAnime.map((anime) => (
+          <a href={anime.url} target='_blank' rel='noreferrer' key={anime.mal_id}>
+            {anime.title}
+          </a>
+        ))}
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;
